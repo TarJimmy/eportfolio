530da0f80e9cb52c6802042309f0a30eece8a2bc
TarJimmy <tardyjim26@gmail.com>
2020-01-17 19:36:06 +0100

    Initialisation dépot

---
+++ view/accueil.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <link href="../framework/bootstrap-4.4.1-dist/css/bootstrap.min.css" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+  </head>
+  <body>
+    <ul class="nav nav-pills">
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link active" href="#">Expérience Professionnelle</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Parcours scolaires</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Projet universitaire</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Projet personnel</a>
+      </li>
+    </ul>
+    <embed src="images/CV.pdf" width="680px" height="1000px">
+  </body>
+</html>
